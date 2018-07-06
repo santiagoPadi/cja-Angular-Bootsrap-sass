945c66cd7e6d9d949256fca8a282b9707712d888
santiago padi <santipachon4@gmail.com>
2018-07-06 17:31:20 -0500

    new CJA project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InstituteComponent } from './components/pages/institute/institute.component';
+import { CalendarComponent } from './components/pages/calendar/calendar.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { NavbarComponent } from './components/general/navbar/navbar.component';
+import { FooterComponent } from './components/general/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstituteComponent,
+    CalendarComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
