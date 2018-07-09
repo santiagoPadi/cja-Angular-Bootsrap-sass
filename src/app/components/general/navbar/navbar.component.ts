c7ee7f436a8c0f18244ab6366764e05f7d6be1ed
santiago padi <santipachon4@gmail.com>
2018-07-09 17:46:53 -0500

    institute alpha 0.1

--- src/app/components/general/navbar/navbar.component.ts
+++ src/app/components/general/navbar/navbar.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  lis = require('./../../../../assets/jsons/navbarLis.json')
+  lis =
+  [
+    {
+      'routerLink': 'home',
+      'name': 'Inicio'
+    },
+    {
+      'routerLink': 'institute',
+      'name': 'Institución'
+    },
+    {
+      'routerLink': 'ingreso',
+      'name': 'Ingreso CJA'
+    },
+    {
+      'routerLink': 'calendar',
+      'name': 'Calendario'
+    },
+    {
+      'routerLink': 'contact',
+      'name': 'Contactenos'
+    }
+  ]
   constructor() {
   }
 
--- src/app/components/pages/home/events/events.component.ts
+++ src/app/components/pages/home/events/events.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
-  nextEvents = require('./../../../../../assets/jsons/nextEvents.json')
+  nextEvents =
+  [
+    {
+        'name': 'Escuela de padres',
+        'day': '09',
+        'month': 'Jul',
+        'hour': '16:00'
+    },
+    {
+        'name': 'Día de la familia',
+        'day': '15',
+        'month': 'Aug',
+        'hour': '8:00'
+    },
+    {
+        'name': 'Inicio vacaciones',
+        'day': '01',
+        'month': 'oct',
+        'hour': '00:00'
+    },
+    {
+      'name': 'inicio ciclo-2',
+      'day': '15',
+      'month': 'nov',
+      'hour': '07:00'
+    }
+]
   mouse = false
   constructor() {
     console.log(this.nextEvents.length)
